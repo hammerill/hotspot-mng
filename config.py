c1c6214a228c6209e64fc4e44fa215756edcc971
Hammerill <www.hammerill@gmail.com>
2023-08-18 16:32:13 +0200

    Update config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Module containing some configs.
 
 HOST_TO_CHECK = "1.1.1.1"
-HOST_CHECK_TIMEOUT = 4 # Seconds
+HOST_CHECK_TIMEOUT = 6 # Seconds
 
-INET_PERIOD = 5 # Seconds
-INET_RETRIES = 3 # Times
+INET_PERIOD = 6 # Seconds
+INET_RETRIES = 4 # Times
 
 AIRPLANE_THRESHOLD = 10 # Seconds
